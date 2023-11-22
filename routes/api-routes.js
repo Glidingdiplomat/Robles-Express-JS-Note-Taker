89ccda2569505799c8a3299ce4c19f873a1bf68b
Glidingdiplomat <yankeefan1246@gmail.com>
2023-11-22 14:34:56 -0500

    more lines to api routes & server.js
    added more lines to api routes and server.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -7,6 +7,14 @@ router.get("/api/notes", async (req, res) => {
   res.json(dbJson);
 });
 
-router.post('/api/notes', (req, res) => {
-    const dbJson = JSON.parse(fs.readFileSync('db/db.json',)
-}
+router.post("/api/notes", (req, res) => {
+  const dbJson = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+  const newFeedback = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uuidv4(),
+  };
+  dbJson.push(newFeedback);
+  fs.writeFileSync("db/db.json", JSON.stringify(dbJson));
+  res.json(dbJson);
+});
